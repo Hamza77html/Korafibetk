155f1c9b13934d0ee8aca73014f74c6c24222b72
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-03-11 22:04:09 +0200

    H

--- Main.js
+++ Main.js
@@ -1,3 +1,10 @@
-function dosomething() {
-    document.getElementsByClassName("Match").INNERHTML = "المصري مباشر الأهلي"
+let btnyes = document.getElementsByClassName ('yes')
+let btntod = document.getElementsByClassName ('tod')
+let btntom = document.getElementsByClassName ('tom')
+let btnnow = document.getElementsByClassName ('now')
+let match = document.querySelector('.match')
+
+btnyes.onclick =function(){
+    match.classList.add('hide')
+    this.classList.add('hide')
 }
