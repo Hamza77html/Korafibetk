30a40f4eabaab192b2d9d271b8678e791dc6522d
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-03-16 02:42:54 +0200

    ف

--- Main.js
+++ Main.js
@@ -1,6 +1,2 @@
-/* Toggle Navbar on Small Screens */
-function toggleNavbar() {
-  var navbar = document.getElementById("myNavbar");
-    if (navbar.className === "collapse navbar-collapse") 
-        navbar
+
 
